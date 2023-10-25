60d69940a5fdc88f5d37724d7971a24481fcd435
Alex Sherman <asherman.ca@gmail.com>
2023-10-25 11:49:41 -0700

    component - dropdown auth

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { ThemeSwitcher } from "./dark-mode/Switcher";
 import Dropdown from "./ui/Dropdown";
+import Link from "next/link";
 
 const Nav = async () => {
   const supabase = createServerComponentClient({
@@ -12,12 +13,14 @@ const Nav = async () => {
     await supabase.auth.getSession();
 
   return (
-    <div className="flex items-center justify-between p-4">
-      <p className="text-2xl font-semibold">Liquify</p>
+    <div className="flex items-center justify-between p-8">
+      <p className="text-3xl font-bold">Liquify</p>
       <div className="flex items-center gap-4">
         <ThemeSwitcher />
-        {sessionData.session?.user.user_metadata && (
+        {sessionData.session?.user.user_metadata ? (
           <Dropdown user={sessionData.session?.user.user_metadata} />
+        ) : (
+          <Dropdown />
         )}
       </div>
     </div>
--- components/ui/Dropdown.tsx
+++ components/ui/Dropdown.tsx
@@ -8,24 +8,49 @@ import {
   Avatar,
 } from "@nextui-org/react";
 import { UserMetadata } from "@supabase/gotrue-js";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface DropdownProps {
-  user: UserMetadata;
+  user?: UserMetadata;
 }
 
 const Dropdown = ({ user }: DropdownProps) => {
-  const { avatar_url } = user;
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  // const { avatar_url } = user;
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  const handleLink = (url: string) => {
+    router.push(url);
+  };
 
   return (
     <div className="flex items-center gap-4">
       <NextDropdown placement="bottom-end">
         <DropdownTrigger>
-          <Avatar as="button" isBordered showFallback src={avatar_url} />
+          <Avatar
+            as="button"
+            isBordered
+            showFallback
+            src={user?.avatar_url || ""}
+          />
         </DropdownTrigger>
         <DropdownMenu aria-label="Profile Actions" variant="flat">
           <DropdownItem href="/profile">Profile</DropdownItem>
           <DropdownItem href="/settings">Settings</DropdownItem>
-          <DropdownItem href="/logout">Logout</DropdownItem>
+          {user ? (
+            <DropdownItem onClick={handleSignOut}>Logout</DropdownItem>
+          ) : (
+            <DropdownItem onClick={() => handleLink("/login")}>
+              Login
+            </DropdownItem>
+          )}
         </DropdownMenu>
       </NextDropdown>
     </div>
