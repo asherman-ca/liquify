53dee0ede6be95545c76210a88c904d0bad60d2b
Alex Sherman <asherman.ca@gmail.com>
2023-10-26 17:34:39 -0700

    refactor - searchdropdown

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -17,7 +17,7 @@ const Nav = async () => {
   const coins = await getCoins();
 
   return (
-    <div className="border-b-1 relative flex items-center justify-between border-gray-300 px-10 py-6">
+    <div className="border-b-1 flex items-center justify-between border-gray-300 px-10 py-6">
       <NavTitle />
       <div className="flex items-center gap-8">
         <NavSearchDropdown coins={coins} />
--- components/NavSearchDropdown.tsx
+++ components/NavSearchDropdown.tsx
@@ -2,6 +2,8 @@
 import { FC, useEffect, useRef, useState } from "react";
 import { Input } from "@nextui-org/react";
 import { Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 interface NavSearchDropdownProps {
   coins: Coin[];
@@ -10,10 +12,14 @@ interface NavSearchDropdownProps {
 const NavSearchDropdown: FC<NavSearchDropdownProps> = ({ coins }) => {
   const [search, setSearch] = useState<string>("");
   const componentRef = useRef<HTMLDivElement | null>(null);
+  const [open, setOpen] = useState<boolean>(false);
 
-  const filteredCoins = coins.filter((coin) => {
-    return coin.name.toLowerCase().includes(search.toLowerCase());
-  });
+  const filteredCoins =
+    search === ""
+      ? coins
+      : coins.filter((coin) => {
+          return coin.name.toLowerCase().includes(search.toLowerCase());
+        });
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -22,7 +28,7 @@ const NavSearchDropdown: FC<NavSearchDropdownProps> = ({ coins }) => {
         !componentRef.current.contains(event.target as Node)
       ) {
         console.log("Clicked outside the component");
-        setSearch("");
+        setOpen(false);
       }
     }
     document.addEventListener("click", handleClickOutside);
@@ -32,17 +38,45 @@ const NavSearchDropdown: FC<NavSearchDropdownProps> = ({ coins }) => {
   }, []);
 
   return (
-    <div ref={componentRef}>
+    <div ref={componentRef} className="relative">
       <Input
+        onClick={() => setOpen(true)}
         placeholder="Search for an asset"
         radius="full"
         value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        onChange={(e) => {
+          setOpen(true);
+          setSearch(e.target.value);
+        }}
         startContent={<Search className="h-5 w-5" strokeWidth={3} />}
+        className="min-w-[250px]"
       />
-      <div id="coin dropdown" className="absolute top-[100%]">
-        {search && filteredCoins.map((coin) => <div id={coin.id}>coin</div>)}
-      </div>
+      {open && (
+        <div
+          id="coin dropdown"
+          className="absolute top-[100%] z-10 mt-4 flex w-full flex-col rounded-lg bg-white p-2 shadow-xl"
+        >
+          {filteredCoins.map((coin) => (
+            <Link
+              href={`/coin/${coin.id}`}
+              id={coin.id}
+              className="flex items-center justify-between rounded-md p-4 hover:bg-gray-100"
+            >
+              <div className="flex items-center gap-2 ">
+                <Image
+                  src={`/icon/${coin.symbol}.png`}
+                  alt="logo"
+                  height={24}
+                  width={24}
+                  className="h-6 w-6"
+                />
+                <p className="text-medium font-semibold">{coin.name}</p>
+              </div>
+              <p>{coin.symbol}</p>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
