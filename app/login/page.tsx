0b998973106ff0be36ddf4cba75918f8c73c56f3
Alex Sherman <asherman.ca@gmail.com>
2023-11-01 10:57:32 -0700

    fix type error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const getURL = () => {
-  let url = process?.env?.NEXT_PUBLIC_SITE_URL;
+  let url = process?.env?.NEXT_PUBLIC_SITE_URL as string;
   // ?? // Set this to your site URL in production env.
   // process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
   // "http://localhost:3000/";
